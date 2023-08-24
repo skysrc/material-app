a790944c9e4705d2c4f8c7cbf796eda3da804956
Sky <skymsdn@hotmail.com>
2023-08-24 23:20:21 +0800

    added responsiveness to sidenav

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
+const SMALL_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
+  public isScreenSmall: boolean = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+  }
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      // .observer([Breakpoints.XSmall])
+      .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
+  }
 
 }
