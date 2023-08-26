bfbd9a5bd2eeefaacda8bed7edcb308c80049b2c
Sky <skymsdn@hotmail.com>
2023-08-27 00:39:01 +0800

    toolbar to emit toggleSidenav event when button is clicked sidenav to listen for toggleSidenav event added margin to main content
    changes as before

--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
+
+  @Output() toggleSidenav = new EventEmitter<void>();
+
+  ngOnInit() {
+
+  }
 
 }
